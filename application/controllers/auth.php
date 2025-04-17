b93703bb54db3a4c96e13db9dfb469ca842a81ec
ZaizafunHanifah <hanifah9158@gmail.com>
2025-04-17 16:36:36 +0700

    update

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -29,10 +29,7 @@ class Auth extends CI_Controller {
         }
     }
 
-    public function logout() {
-        $this->session->sess_destroy();
-        redirect('auth');
-    }
+
 
     public function register() {
         $this->load->view('register');
